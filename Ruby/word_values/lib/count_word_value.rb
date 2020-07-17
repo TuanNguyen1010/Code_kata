dc0d1609d345b5a2ce576795ee814322302ebd69
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-07-17 13:50:51 +0100

    Refactor: remove magic number

--- Ruby/word_values/lib/count_word_value.rb
+++ Ruby/word_values/lib/count_word_value.rb
@@ -1,9 +1,10 @@
 def count_word_value(array)
   new_arr = []
+  codepoint_before_letter = 96
   array.each {|string|
     arr = string.split('')
     total = 0
-    arr.each {|ch| total += ch.ord - 96}
+    arr.each {|ch| total += ch.ord - codepoint_before_letter}
     new_arr.push(total)
   }
   return new_arr
