a81320a1e29cd43652dfab937fd03c582bccfdda
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-07-17 14:03:28 +0100

    Refactor: use map instead of each

--- Ruby/word_values/lib/count_word_value.rb
+++ Ruby/word_values/lib/count_word_value.rb
@@ -1,15 +1,12 @@
 def count_word_value(array)
-  new_arr = []
   codepoint_before_letter = 96
-  array.each {|string|
-    arr = string.split('')
+  array.map {|string|
     total = 0
-    arr.each {|ch| 
-    if(ch != ' ')
-      total += ch.ord - codepoint_before_letter
-    end 
+    string.split('').each {|ch| 
+      if(ch != ' ')
+        total += ch.ord - codepoint_before_letter
+      end
     }
-    new_arr.push(total)
+    total
   }
-  return new_arr
 end 
