d858aa568507de3808b6e7f010caba5302132a46
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-07-18 16:52:23 +0100

    Test: All code Kata cases

--- Ruby/calculate_distance/spec/calculate_distance_spec.rb
+++ Ruby/calculate_distance/spec/calculate_distance_spec.rb
@@ -8,4 +8,8 @@ describe '#calculate distance' do
   it('calculate distance from point [5,4] and [3,2]') do
     expect(calculate_distance([5,4], [3,2])).to eq(4)
   end 
+
+  it('calculate distance from [1,1] and [0,3]') do
+    expect(calculate_distance([1,1], [0,3])).to eq(3)
+  end 
 end 
