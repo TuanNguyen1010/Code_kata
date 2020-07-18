bc192d5779fe53f42abe114797a06ec39a51d063
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-07-18 21:02:02 +0100

    Style: fix indentation

--- Ruby/calculate_mistake/lib/calculate_mistake.rb
+++ Ruby/calculate_mistake/lib/calculate_mistake.rb
@@ -11,15 +11,15 @@ def calculate_mistake(a, b)
   new_arr = []
   index_point = long_string.length - short_string.length
 
-    long_string.each_with_index {|number, index|
-      if (index_point > 0 && index_point > index)
-        new_arr.push(number)
-      elsif (index_point > 0)
-        new_arr.push(number.to_i + short_string[index-index_point].to_i)
-      else 
-        new_arr.push(number.to_i + short_string[index].to_i)
-      end 
-    } 
+  long_string.each_with_index {|number, index|
+    if (index_point > 0 && index_point > index)
+      new_arr.push(number)
+    elsif (index_point > 0)
+      new_arr.push(number.to_i + short_string[index-index_point].to_i)
+    else 
+      new_arr.push(number.to_i + short_string[index].to_i)
+    end 
+  } 
 
   return new_arr.join('').to_i
 end 
