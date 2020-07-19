37caafb0020e79ac7c6f2caf7a4fce882cb5b96f
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-07-19 15:32:13 +0100

    Test: second passing case

--- Ruby/character_with_longest_repetition/lib/count_char.rb
+++ Ruby/character_with_longest_repetition/lib/count_char.rb
@@ -1,3 +1,19 @@
 def count_char(string)
-  return ['a', 2]
+  new_string = string + ' '
+  new_arr = new_string.split('')
+  current_letter =' '
+  current_count = 0
+  highest_count = ['', 0]
+  new_arr.each {|letter|
+  if (letter == current_letter)
+    current_count += 1
+  elsif (letter != current_letter)
+      if ( highest_count[1] < current_count)
+        highest_count = [current_letter, current_count]
+      end 
+      current_letter = letter
+      current_count = 1
+  end 
+  }
+  return highest_count
 end 
--- Ruby/character_with_longest_repetition/spec/count_char_spec.rb
+++ Ruby/character_with_longest_repetition/spec/count_char_spec.rb
@@ -4,4 +4,20 @@ describe('#count_char') do
   it('takes a string of 2 indices') do
     expect(count_char('aa')).to eq(['a', 2])
   end 
+
+  it('takes a string with 2 different letters') do
+    expect(count_char('abb')).to eq(['b', 2])
+  end
+
+  it('takes in a string with 4 indices') do 
+    expect(count_char(("aaaabb"))).to eq(['a', 4])
+  end 
+
+  it('takes an string with multiple different letters') do 
+    expect(count_char('bbbaaabaaaaac')).to eq(['a', 5])
+  end
+
+  it('takes an empty string') do
+    expect(count_char('')).to eq(['', 0])
+  end 
 end 
