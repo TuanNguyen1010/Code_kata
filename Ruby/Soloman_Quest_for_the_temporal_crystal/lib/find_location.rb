e17a7c73b252374ee70b0d3904113bbd703ded3d
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-07-19 12:17:16 +0100

    Refactor: add variables for clarity

--- Ruby/Soloman_Quest_for_the_temporal_crystal/lib/find_location.rb
+++ Ruby/Soloman_Quest_for_the_temporal_crystal/lib/find_location.rb
@@ -2,16 +2,19 @@ def find_location(arr)
   current_location = [0,0]
   current_dilation = 0
   arr.each{|move|
+  x_coordinates = current_location[0]
+  y_coordinates = current_location[1]
   current_dilation += move[0]
   distance = (2 ** current_dilation) * move[2]
   if (move[1] == 0)
-  return current_location = [0, distance]
+    current_location[1] = y_coordinates + distance
   elsif (move[1] == 1)
-  return current_location= [distance, 0]
+    current_location[0]= x_coordinates + distance
   elsif (move[1] == 2)
-  return current_location = [0, 0 - distance]
-  else
-  return current_location = [0- distance, 0]
-  end
+    current_location[1] = y_coordinates - distance
+  else 
+    current_location[0] = x_coordinates - distance
+  end 
   }
-end
+  return current_location
+end 
