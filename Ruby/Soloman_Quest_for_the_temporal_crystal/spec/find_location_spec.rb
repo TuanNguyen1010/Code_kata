3f914bc17ced0ce6b1bac16472be4dfecdb01e2a
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-07-19 12:19:00 +0100

    Test: add test case from CodeWars

--- Ruby/Soloman_Quest_for_the_temporal_crystal/spec/find_location_spec.rb
+++ Ruby/Soloman_Quest_for_the_temporal_crystal/spec/find_location_spec.rb
@@ -4,4 +4,8 @@ describe '#find_location' do
   it('receives an array of 1 element') do 
     expect(find_location([[1, 3, 5]])).to eq([-10,0])
   end 
+
+  it('receives a range of arrays inside arrays') do
+    expect(find_location([[1,3,5],[2,0,10],[-3,1,4],[4,2,4],[1,1,5],[-3,0,12],[2,1,12],[-2,2,6]])).to eq([346,40])
+  end 
 end 
