a0018de19013ef721404c967c11a8b4863b4c0f2
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-07-21 10:13:51 +0100

    Refactor: change if statement to loop

--- Ruby/english_beggers/lib/calculate_begger_total.rb
+++ Ruby/english_beggers/lib/calculate_begger_total.rb
@@ -1,7 +1,7 @@
 def calculate_begger_total(values, n)
-  if(values == [1])
-    return [1]
-  else 
-    return [15]
-  end 
+  array = [0]
+  values.each {|single|
+    array[0] +=  single
+  }
+  return array
 end 
