4959935bf76f94d167d877bb1e6bbbe611c1a6a7
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-08-11 16:14:27 +0100

    Test: all Code Kata case passing'

--- Ruby/digital_cypher_vol_3/spec/decypher_code_spec.rb
+++ Ruby/digital_cypher_vol_3/spec/decypher_code_spec.rb
@@ -8,4 +8,8 @@ describe('#decypher_code') do
   it('input masterpeice and array and return code 1938') do 
     expect(decypher_code('masterpiece', [ 14, 10, 22, 28, 6, 27, 19, 17, 6, 12, 8])).to eq(1938)
   end 
+
+  it('input nomoretears along with array and return 12') do
+    expect(decypher_code("nomoretears", [15, 17, 14, 17, 19, 7, 21, 7, 2, 20, 20])).to eq(12)
+  end 
 end 
