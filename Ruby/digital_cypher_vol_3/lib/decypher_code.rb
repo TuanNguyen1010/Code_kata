7e91bab81992faa32249bed606e4c739907d1d83
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-08-11 16:11:57 +0100

    Test: passing 11 element array

--- Ruby/digital_cypher_vol_3/lib/decypher_code.rb
+++ Ruby/digital_cypher_vol_3/lib/decypher_code.rb
@@ -1,3 +1,18 @@
-def decypher_code(word, arr)
-  return 1939
+def decypher_code(message, code)
+  array = convert_string_to_number(message)
+  new_array = []
+  code.each_with_index { |v, i|
+  new_array.push(v - array[i])
+  }
+  arr = new_array.join()
+  len = (1..code.length).find { |x| 
+  (arr[0, x] * code.length)[0, code.length] == arr }
+  arr[0, len].to_i
+end
+
+def convert_string_to_number(string)
+  codepoint_before_letter = 96
+  string.split('').map {|l| 
+  l.ord - codepoint_before_letter 
+}
 end 
--- Ruby/digital_cypher_vol_3/spec/decypher_code_spec.rb
+++ Ruby/digital_cypher_vol_3/spec/decypher_code_spec.rb
@@ -4,4 +4,8 @@ describe('#decypher_code') do
   it('input scout and array, returns code') do
     expect(decypher_code('scout', [ 20, 12, 18, 30, 21])).to eq(1939)
   end 
+
+  it('input masterpeice and array and return code 1938') do 
+    expect(decypher_code('masterpiece', [ 14, 10, 22, 28, 6, 27, 19, 17, 6, 12, 8])).to eq(1938)
+  end 
 end 
