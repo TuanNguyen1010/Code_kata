331e4802175afc2ac47674c45988ea24aeec608f
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-08-13 19:44:49 +0100

    Test: second passing test

--- Ruby/swap_case_using_n/lib/spec_swap.rb
+++ Ruby/swap_case_using_n/lib/spec_swap.rb
@@ -1,3 +1,23 @@
 def spec_swap(string, n)
-  return "heLLO wORLd!"
+  new_arr = []
+  new_arr_n = ((n.to_s(2) * string.length).split(''))[0, string.length]
+
+  index = 0
+  string.split('').each_with_index { |v, i|
+    if(letter?(v) == nil)
+      new_arr.push(v)
+    elsif(new_arr_n[index] == '1')
+      new_arr.push(v.swapcase)
+      index += 1
+    else
+      new_arr.push(v)
+      index += 1
+    end
+  }
+
+  return new_arr.join()
 end 
+
+def letter?(char)
+  char =~ /[[:alpha:]]/
+end
--- Ruby/swap_case_using_n/spec/spec_swap_spec.rb
+++ Ruby/swap_case_using_n/spec/spec_swap_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_swap'
 
 describe("#spec_swap") do 
+
+  it('accepts Hello and 11 input') do
+    expect(spec_swap('Hello', 11)).to eq('heLLO')
+  end
+
   it('accepts Hello world and swap to lowercase base on n') do
     expect(spec_swap("Hello world!", 11)).to eq("heLLO wORLd!")
   end 
+  
 end 
