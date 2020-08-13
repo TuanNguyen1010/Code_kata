925f1d1ed2e81e73ce02159f7b6dfd16f3b1d5e0
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-08-13 19:46:58 +0100

    Test: edge case

--- Ruby/swap_case_using_n/spec/spec_swap_spec.rb
+++ Ruby/swap_case_using_n/spec/spec_swap_spec.rb
@@ -14,4 +14,8 @@ describe("#spec_swap") do
   it('takes a string containing every letter and swap to lower case base on n') do
     expect(spec_swap("the quick broWn fox leapt over the fence", 9)).to eq("The QUicK BrowN foX LeaPT ovER thE FenCE")
   end 
+
+  it('takes an empty string and returns empty string') do
+    expect(spec_swap("", 11345)).to eq('')
+  end 
 end 
