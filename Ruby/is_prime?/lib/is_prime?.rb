ddaeab445bedade106d00aef1bbcce8bdff5051e
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-08-17 14:27:24 +0100

    Test: case for 999

--- Ruby/is_prime?/lib/is_prime?.rb
+++ Ruby/is_prime?/lib/is_prime?.rb
@@ -1,5 +1,7 @@
 def is_prime?(number)
-  if number <= 0
+  if number < 5 && number > 0
+    return true
+  elsif number <= 0 
     return false
   elsif number % 2 == 0 || number % 3 == 0
     return false
